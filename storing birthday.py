350090d8989bacf4883c43f031d2c5a4f1ed96fe
sabib112 <herringbone.bd@gmail.com>
2025-01-03 17:06:48 +0600

    storing birthday

---
+++ storing birthday.py
@@ -0,0 +1,13 @@
+print("Storing birthdays")
+ayman="21st may 2012"
+purbayun="6th february 2013"
+sabib="1st january 2012"
+tairan="16th december 2011"
+
+print("Birthday of Ayman: ",ayman)
+
+print("Birthday of Purbayun: ",purbayun)
+
+print("Birthday of Sabib: ",sabib)
+
+print("Birthday of Tairan: ",tairan)

30609cf3eed44053588a895074bb210b024a30aa
sabib112 <herringbone.bd@gmail.com>
2025-01-16 16:48:41 +0600

    operation 2

---
+++ and or operator.py
@@ -0,0 +1,28 @@
+#and
+a=1
+b=1
+c=0
+if a and b and c:
+    print ("all are true")
+else:
+    print("not all are true")
+
+a=12
+b=5
+c=10
+
+if a>0 and b>0 and c>0:
+    print("all are positive")
+
+else:
+    print ("not all are positive")
+
+
+a=-10
+b=-5
+c=-12
+
+if a>0 or b>0 or c>0:
+    print("atleast one number is positive")
+else:
+    print("no number is positive")
---
+++ not equal.py
@@ -0,0 +1,26 @@
+a=10
+b=2
+c=130
+
+print(a != b)
+
+if a != b:
+    print(" a is not equal to b")
+
+else:
+    print("a and b is equal")
+
+a="Python"
+b="python"
+
+if a != b:
+    print("none word are equal")
+else:
+    print("all words are equal")
+
+num = int(input("enter your number: "))
+
+if num%2:
+    print("number is odd")
+else:
+    print("number is even")

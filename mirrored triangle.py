fe6fd3b2ad7ac4621f34317324e10d869719c83e
Sabib112 <herringbone.bd@gmail.com>
2025-02-14 15:15:02 +0600

    numbber tiangle

---
+++ mirrored triangle.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number of rows: "))
+print("Mirrored half pyramid with stars (*)")
+
+for i in range(n):
+
+    for j in range(n-i-1):
+        print(" ", end="")
+
+    for k in range(i+1):
+        print("*", end="")
+    print("")
